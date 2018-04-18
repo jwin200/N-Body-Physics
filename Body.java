dfeb4e8a4e3713b2377b0b2ee11339de79208548
jwin200 <38480717+jwin200@users.noreply.github.com>
2018-04-18 11:25:15 -0400

    Update Body.java

--- Body.java
+++ Body.java
@@ -8,32 +8,32 @@ public class Body {
 	private double dir;
 	private Color color;
 	private Point loc;
-	private Vector body;
+	private Vector vector;
 	private double radius;
 	
 	public double mass() { return mass; }
 	public double speed() { return speed; }
 	public double dir() { return dir; }
-	public Vector vector() { return body; }
+	public Vector vector() { return vector; }
 	public double radius() { return radius; }
 	public Point loc() { return loc; }
 	
 	
 	public Body(int mass, double speed, double dir, Color color, Point loc) {
 		this.mass = mass;
-		body = new Vector(speed, dir);
+		vector = new Vector(speed, dir);
 		this.color = color;
 		this.loc = loc;
 	}
 	
 	public void step(Vector net) {
-		body = body.add(net);
-		loc = new Point(loc.x() + body.xVel(), loc.y() + body.yVel());
+		vector = vector.add(net);
+		vector.update();
+		loc = new Point(loc.x() + vector.xVel(), loc.y() + vector.yVel());
 	}
 	
 	public void draw() {
 		StdDraw.setPenColor(color);
-		//StdDraw.setPenColor(StdDraw.color);
 		double radius = Math.sqrt(Math.sqrt(mass)/Math.PI);
 		StdDraw.filledCircle(loc.x(), loc.y(), radius);
 		}	
