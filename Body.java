2ac202da9af85b27d4068c7d6ae1e4bdfc291e2a
NIckmanv <38499016+NIckmanv@users.noreply.github.com>
2018-04-18 10:28:02 -0400

    Update Body.java

--- Body.java
+++ Body.java
@@ -1,4 +1,32 @@
+import java.awt.Color;
 
-public abstract class Body {
+public class Body {
 
+	//fields
+	int mass;
+	double speed;
+	double dir;
+	Color color;
+	Point loc;
+	Vector body;
+	double radius;
+	
+	
+	public Body(int mass, double speed, double dir, Color color, Point loc) {
+		this.mass = mass;
+		body = new Vector(speed, dir);
+		this.color = color;
+		this.loc = loc;
+	}
+	
+	public void step(Vector net) {
+		body = body.add(net);
+		loc = new Point(loc.x() + body.xVel(), loc.y() + body.yVel());
+	}
+	
+	public void draw() {
+		//StdDraw.setPenColor(StdDraw.color);
+		double radius = Math.sqrt(Math.sqrt(mass)/Math.PI);
+		StdDraw.filledCircle(loc.x(), loc.y(), radius);
+		}	
 }
