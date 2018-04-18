553ab9d047a828a6492d61be5da6de4ddf1edde2
jwin200 <38480717+jwin200@users.noreply.github.com>
2018-04-18 11:24:51 -0400

    Update Universe.java

--- Universe.java
+++ Universe.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class Universe {
 	public static List<Body> bodies;
-	public static final double G = 6.674 * Math.pow(10, -11);
+	public static final double G = 6.674 * Math.pow(10, -7);
 	
 	// Constructor
 	public Universe() {
@@ -11,19 +11,19 @@ public class Universe {
 	
 	// Methods
 	/** Runs the universe. */
-	public static void run() {
+	public void run() {
 		StdDraw.enableDoubleBuffering();
 		StdDraw.setXscale(0, 100);
 		StdDraw.setYscale(0, 100);
 		while(true) {
 			draw();
 			step();
-			//System.out.println(bodies.get(0).loc());
+			System.out.println(bodies.get(1).vector());
 		}
 	}
 	
 	/** Draws each object in the bodies List. */
-	public static void draw() {
+	public void draw() {
 		StdDraw.clear();
 		for(Body body : bodies) {
 			body.draw();
@@ -32,7 +32,7 @@ public class Universe {
 	}
 	
 	/** Steps each object in the bodies List. */
-	public static void step() {
+	public void step() {
 		// Cycling through each body
 		for(int i = 0; i < bodies.size(); i++) {
 			List<Vector> forces = new ArrayList<>();
@@ -40,7 +40,7 @@ public class Universe {
 			// Creating a list of individual forces acting upon this body
 			for(Body body : bodies) {
 				if(body != bodies.get(i)) {
-					double gravity = calcForce(bodies.get(i).mass(), body.mass(), body.loc().distanceTo(bodies.get(i).loc()));
+					double gravity = -calcForce(bodies.get(i).mass(), body.mass(), body.loc().distanceTo(bodies.get(i).loc())) / bodies.get(i).mass();
 					double direction = body.loc().dirTo(bodies.get(i).loc());
 					forces.add(new Vector(gravity, direction));
 				}
@@ -52,18 +52,18 @@ public class Universe {
 	}
 	
 	/** Calculating the force of gravity between two objects. */
-	public static double calcForce(double mass1, double mass2, double dist) {
+	public double calcForce(double mass1, double mass2, double dist) {
 		double num = G * mass1 * mass2;
 		double denom = Math.pow(dist, 2);
 		return num / denom;
 	}
 	
-	public static void add(Body body) { bodies.add(body); }
+	public void add(Body body) { bodies.add(body); }
 	
 	public static void main(String[] args) {
 		Universe universe = new Universe();
-		 universe.add(new Body(100000, 0.0001, .6, StdDraw.BLACK, new Point(50, 50)));
-		 
+		 universe.add(new Body(100, 0.01, Math.PI, StdDraw.BLACK, new Point(55, 80)));
+		 universe.add(new Body(10000, 0.000000001, 0, StdDraw.MAGENTA, new Point(50, 50)));
 		 universe.run();
 	}
 }
