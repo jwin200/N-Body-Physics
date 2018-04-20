9793d3911d08c7c6dec1648e1c983142463fbcab
jwin200 <38480717+jwin200@users.noreply.github.com>
2018-04-19 20:29:06 -0400

    Update Body.java

--- Body.java
+++ Body.java
@@ -1,40 +1,58 @@
 import java.awt.Color;
 
+/**
+ * An object representative of celestial bodies
+ * to exist in a universe. Stores data for mass,
+ * velocity, size, and location.
+ * 
+ * @author Jonah Winchell & Nick Schneider
+ * @version April 19, 2018
+ */
 public class Body {
 
-	//fields
-	private double mass;
-	private double speed;
-	private double dir;
+	// Fields
+	private double mass, speed, dir, radius;
 	private Color color;
 	private Point loc;
 	private Vector vector;
-	private double radius;
 	
 	public double mass() { return mass; }
 	public double speed() { return speed; }
 	public double dir() { return dir; }
-	public Vector vector() { return vector; }
 	public double radius() { return radius; }
+	public Vector vector() { return vector; }
 	public Point loc() { return loc; }
 	
-	
-	public Body(int mass, double speed, double dir, Color color, Point loc) {
+	// Constructors
+	public Body(double mass, double speed, double dir, Color color, Point loc) {
 		this.mass = mass;
 		vector = new Vector(speed, dir);
 		this.color = color;
 		this.loc = loc;
 	}
 	
+	public Body(double mass, Vector vector, Point loc) {
+		this.mass = mass;
+		this.vector = vector;
+		color = StdDraw.BLACK;
+		this.loc = loc;
+	}
+	
+	// Methods
+	/** Animates the body based on its vector. */
 	public void step(Vector net) {
 		vector = vector.add(net);
-		vector.update();
 		loc = new Point(loc.x() + vector.xVel(), loc.y() + vector.yVel());
 	}
 	
+	/** Draws this body. */
 	public void draw() {
 		StdDraw.setPenColor(color);
 		double radius = Math.sqrt(Math.sqrt(mass)/Math.PI);
 		StdDraw.filledCircle(loc.x(), loc.y(), radius);
-		}	
+	}
+	
+	public String toString() {
+		return loc + " " + vector;
+	}
 }
