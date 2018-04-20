2a5be0bfb1e4ee8a5c0edbc8177768f0e206d50a
jwin200 <38480717+jwin200@users.noreply.github.com>
2018-04-19 20:29:33 -0400

    Update Universe.java

--- Universe.java
+++ Universe.java
@@ -1,8 +1,21 @@
 import java.util.*;
 
+/**
+ * A universe of bodies interacting with each other's
+ * gravitational fields. Orbits and other interactions
+ * are modeled.
+ * 
+ * @author Jonah Winchell & Nick Schneider
+ * @version April 19, 2018
+ */
 public class Universe {
+	// List of all bodies in the universe
 	public static List<Body> bodies;
-	public static final double G = 6.674 * Math.pow(10, -7);
+	// Newton's constant (exponent altered for scaling)
+	public static final double G = 6.674 * Math.pow(10, -8);
+	// Scales for the program window
+	public static final int XSCALE = 200;
+	public static final int YSCALE = 200;
 	
 	// Constructor
 	public Universe() {
@@ -13,12 +26,15 @@ public class Universe {
 	/** Runs the universe. */
 	public void run() {
 		StdDraw.enableDoubleBuffering();
-		StdDraw.setXscale(0, 100);
-		StdDraw.setYscale(0, 100);
+		StdDraw.setXscale(0, XSCALE);
+		StdDraw.setYscale(0, YSCALE);
 		while(true) {
 			draw();
 			step();
-			System.out.println(bodies.get(1).vector());
+			
+			// Debugging
+			//System.out.println(bodies.get(0));
+			//System.out.println(bodies.get(1));
 		}
 	}
 	
@@ -35,20 +51,27 @@ public class Universe {
 	public void step() {
 		// Cycling through each body
 		for(int i = 0; i < bodies.size(); i++) {
-			List<Vector> forces = new ArrayList<>();
 			Vector force = new Vector(0, 0);
-			// Creating a list of individual forces acting upon this body
+			// Creating a total force vector for this body
 			for(Body body : bodies) {
 				if(body != bodies.get(i)) {
-					double gravity = -calcForce(bodies.get(i).mass(), body.mass(), body.loc().distanceTo(bodies.get(i).loc())) / bodies.get(i).mass();
-					double direction = body.loc().dirTo(bodies.get(i).loc());
-					forces.add(new Vector(gravity, direction));
+					force = force.add(calcVel(bodies.get(i), body));
 				}
 			}
-			// Creating a total vector force for this body
-			for(Vector vector : forces) { force = force.add(vector); }
+			// Imparting that force vector onto the body
 			bodies.get(i).step(force);
 		}
+	}
+	
+	/** Calculating the velocity change imparted by the gravity of two bodies. */
+	public Vector calcVel(Body body1, Body body2) {
+		double distance = body1.loc().distanceTo(body2.loc());
+		double gravity = calcForce(body1.mass(), body2.mass(), distance) / body1.mass();
+		if(body1.loc().x() > body2.loc().x()) { gravity = -gravity; } // This line fixed the code
+		double direction = body1.loc().dirTo(body2.loc());
+		double gravityX = Math.cos(direction) * gravity;
+		double gravityY = Math.sin(direction) * gravity;
+		return new Vector(gravityX, gravityY);
 	}
 	
 	/** Calculating the force of gravity between two objects. */
@@ -58,12 +81,16 @@ public class Universe {
 		return num / denom;
 	}
 	
+	/** Adds a body to the Bodies List. */
 	public void add(Body body) { bodies.add(body); }
 	
 	public static void main(String[] args) {
 		Universe universe = new Universe();
-		 universe.add(new Body(100, 0.01, Math.PI, StdDraw.BLACK, new Point(55, 80)));
-		 universe.add(new Body(10000, 0.000000001, 0, StdDraw.MAGENTA, new Point(50, 50)));
+		 universe.add(new Body(100, new Vector(0, .01), new Point(50, 100)));
+		 universe.add(new Body(100, new Vector(0, -.01), new Point(150, 100)));
+		 universe.add(new Body(1000, new Vector(-.01, 0), new Point(100, 50)));
+		 universe.add(new Body(100000, new Vector(0, 0), new Point(100, 100)));
+		 
 		 universe.run();
 	}
 }
