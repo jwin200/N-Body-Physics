efbea65e55646beaa8f096ae81ecb16bb27dd3bc
jwin200 <38480717+jwin200@users.noreply.github.com>
2018-04-30 15:55:10 -0400

    Update Star.java

--- Star.java
+++ Star.java
@@ -7,15 +7,18 @@ public class Star extends Body {
 	
 	public void draw(Vector force) {
 		if(super.show()) {
+			// Drawing the force on this body
 			StdDraw.setPenColor(StdDraw.RED);
 			double newX = super.loc().x() + (force.xVel() * 1000000);
 			double newY = super.loc().y() + (force.yVel() * 1000000);
 			StdDraw.line(super.loc().x(), super.loc().y(), newX, newY);
+			// Drawing the body's current vector
 			StdDraw.setPenColor(StdDraw.YELLOW);
 			newX = super.loc().x() + (super.vector().xVel() * 1000);
 			newY = super.loc().y() + (super.vector().yVel() * 1000);
 			StdDraw.line(super.loc().x(), super.loc().y(), newX, newY);
 		}
+		// Drawing the body
 		StdDraw.setPenColor(StdDraw.YELLOW);
 		StdDraw.filledCircle(super.loc().x(), super.loc().y(), super.radius());
 	}
